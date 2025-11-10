0a9591472ebd4d3a668610883cad119a2f673ac6
romar <romandva8078@gmail.com>
2025-11-10 09:29:11 +0300

    variable adjustment

--- src/widget.py
+++ src/widget.py
@@ -1,37 +1,33 @@
 # импорт функций маскировки
-from src.masks import get_mask_card_number
-from src.masks import get_mask_account
+from src.masks import get_mask_card_number, get_mask_account
 
 
 def mask_account_card(account_or_card: str) -> str:
     """Функция, маскирующая счет либо номер карты, в зависимости от ввода данных"""
+    parts = account_or_card.split()
+    account_type = " ".join(parts[:-1])
+    number = parts[-1]
 
-    parts = account_or_card.split()  # Определяем тип карты/счета
-    type_str = " ".join(parts[:-1])
-    number = parts[-1]  # Номер (последний элемент)
-
-    if "Счет" in type_str:
-        mask_number = get_mask_account(number)
-        return f"{type_str} {mask_number}"
+    if "Счет" in account_type:
+        masked_number = get_mask_account(number)
+        return f"{account_type} {masked_number}"
     else:
-        mask_number = get_mask_card_number(number)
-        return f"{type_str} {mask_number}"  # выход функции номер карты
+        masked_number = get_mask_card_number(number)
+        return f"{account_type} {masked_number}"
 
 
-account_or_card = input()  # входной аргумент
-print(mask_account_card(account_or_card))
+user_input = input()
+print(mask_account_card(user_input))
 
 
 def get_date(date_and_time: str) -> str:
-    """Функция, переводящая введенную дата и время в формат дд.мм.гггг"""
-    data_or_time = date_and_time.split("T")  # Разделяем дату и время
-    date = data_or_time[0]  # Отделение даты
-    day = date[8:10]
-    month = date[5:7]
-    year = date[0:4]
+    """Функция, переводящая введенную дату и время в формат дд.мм.гггг"""
+    date_part = date_and_time.split("T")[0]
+    year = date_part[:4]
+    month = date_part[5:7]
+    day = date_part[8:10]
     return f"{day}.{month}.{year}"
 
 
-date_and_time = input()  # входной аргумент
-title: str
-print(get_date(date_and_time))
+date_input = input()
+print(get_date(date_input))
