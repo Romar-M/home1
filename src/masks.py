43c60773bba850446e47976cee2dfb05a260f3c0
romar <romandva8078@gmail.com>
2025-11-16 11:48:35 +0300

    generator

--- src/masks.py
+++ src/masks.py
@@ -7,6 +7,11 @@ def get_mask_card_number(card_number: str) -> str:
 
 def get_mask_account(account_number: str) -> str:
     """Функция маскировки банковского счета"""
-    if not account_number or not isinstance(account_number, str) or len(account_number) < 4 or not account_number.isdigit():
+    if (
+        not account_number
+        or not isinstance(account_number, str)
+        or len(account_number) < 4
+        or not account_number.isdigit()
+    ):
         return None
     return "**" + account_number[-4:]
